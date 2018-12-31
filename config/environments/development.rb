2d4400bcbc996f130ea43716418f7b6e921b5e32
p1zzadog <steve.moody@motili.com>
2018-12-31 11:10:40 -0700

    updated pokeql web app port to 3001 inside docker container fixing sprite urls and potentially other areas

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,5 +52,5 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.url = "http://localhost:3000"
+  config.url = "http://localhost:3001"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171031021522) do
+ActiveRecord::Schema.define(version: 2017_10_31_021522) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
